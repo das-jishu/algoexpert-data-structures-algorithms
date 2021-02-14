295b0cc1a1356cc98fa073795c6187f4bd549dce
Subham <das.jishu25@gmail.com>
2021-02-14 08:46:24 +0530

    Adding hard and medium problems

---
+++ Medium/sum-of-linked-lists.py
@@ -0,0 +1,40 @@
+
+# SUM OF LINKED LISTS
+
+# This is an input class. Do not edit.
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+# O(max(N, M)) time and space
+def sumOfLinkedLists(linkedListOne, linkedListTwo):
+    # Write your code here.
+    headOfNew = None
+	currentOfNew = headOfNew
+	currentOne = linkedListOne
+	currentTwo = linkedListTwo
+	carry = 0
+	
+	while currentOne != None or currentTwo != None:
+		digitOne = currentOne.value if currentOne != None else 0
+		digitTwo = currentTwo.value if currentTwo != None else 0
+		
+		value = digitOne + digitTwo + carry
+		carry = 1 if value > 9 else 0
+		
+		newNode = LinkedList(value % 10)
+		if headOfNew is None:
+			headOfNew = newNode
+			currentOfNew = headOfNew
+		else:
+			currentOfNew.next = newNode
+			currentOfNew = currentOfNew.next
+			
+		currentOne = currentOne.next if currentOne != None else currentOne
+		currentTwo = currentTwo.next if currentTwo != None else currentTwo
+	
+	if carry > 0:
+		currentOfNew.next = LinkedList(carry)
+	
+	return headOfNew
